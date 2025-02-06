ab3a45ad21fb31f2841b9a8b2e63d17c26320244
guywhale <43272320+guywhale@users.noreply.github.com>
2025-02-06 12:49:17 +0000

    set up BND template

---
+++ templates/BND/V1.js
@@ -0,0 +1,52 @@
+((w) => {
+    "use strict";
+    const tag = "cv-<<<CLIENT_CODE_LOWERCASE>>>-<<<EXPERIMENT_NUMBER_HYPHENATED>>>";
+    const exp = "<<<CLIENT_CODE>>> <<<EXPERIMENT_NUMBER>>>";
+    const qa = true;
+    //const qa = document.cookie.indexOf('cfQA') > -1;
+    const window = typeof unsafeWindow !== "undefined" ? unsafeWindow : w;
+    const log = qa ? Function.prototype.bind.call(console.log, console, `[CONV] ${exp} |`) : () => {};
+    const logErr = qa ? Function.prototype.bind.call(console.error, console, `[CONV] ${exp} Error |`) : () => {};
+    const imgPath = "https://d1mgcpums0qvsa.cloudfront.net/<<<CLIENT_CODE>>>/<<<EXPERIMENT_NUMBER>>>/";
+    window.VWO = window.VWO || [];
+    VWO.event =
+        VWO.event ||
+        function () {
+            VWO.push(["event"].concat([].slice.call(arguments)));
+        };
+
+    window[tag] = {
+        waitUntil: (condition, wait = 5000) => {
+            return new Promise((resolve, reject) => {
+                let stop;
+
+                const timeout =
+                    wait &&
+                    setTimeout(() => {
+                        stop = true;
+                        reject();
+                    }, wait);
+
+                const check = () => {
+                    if (stop) return;
+                    if (!condition()) return requestAnimationFrame(check);
+
+                    clearTimeout(timeout);
+                    resolve(condition());
+                };
+
+                requestAnimationFrame(check);
+            });
+        },
+    };
+    const utils = window[tag];
+
+    utils
+        .waitUntil(() => document.querySelector("body"), 0)
+        .then((element) => {
+            log("Activated", element);
+        })
+        .catch((err) => {
+            logErr(err);
+        });
+})(window);
